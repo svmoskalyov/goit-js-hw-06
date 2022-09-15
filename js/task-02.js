6a6461205c1e13bcfd89c2170d20785b8635eb4e
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-15 23:51:51 +0300

    task02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientListEl = document.getElementById('ingredients');
+
+const createIngredientListEl = options => {
+  return options.map(item => {
+    const itemIngredientListEl = document.createElement('li');
+    itemIngredientListEl.classList.add('item');
+    itemIngredientListEl.textContent = item;
+    return itemIngredientListEl;
+  });
+};
+
+const elemetns = createIngredientListEl(ingredients);
+ingredientListEl.append(...elemetns);
