0701d7ad7ec7d130dd6daf514379f36b47ee7c9b
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-17 22:52:15 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,10 @@
 const input = document.querySelector('#validation-input');
-const inputDataLength = +document.querySelector('[data-length]').dataset.length;
+const inputDataLength = document.querySelector('[data-length]').dataset.length;
 
 input.addEventListener('blur', onInputBlur);
 
 function onInputBlur() {
-  inputDataLength === input.value.length
+  +inputDataLength === input.value.length
     ? input.classList.add('valid')
     : input.classList.add('invalid');
 }
