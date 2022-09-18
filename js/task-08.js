986fc1293a336917f8e36737e3ab5cd313c67a84
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-18 15:57:04 +0300

    task08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,43 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (mail === '' || password === '') {
+    return console.log('Please fill in all the fields!');
+  }
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  console.log(formData);
+  event.currentTarget.reset();
+}
+
+// --- variant 2
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password },
+//   } = event.currentTarget;
+
+//   if (email.value === '' || password.value === '') {
+//     return console.log('Please fill in all the fields!');
+//   }
+
+//   const formData = {
+//     email: email.value,
+//     password: password.value,
+//   };
+
+//   console.log(formData);
+//   event.currentTarget.reset();
+// }
