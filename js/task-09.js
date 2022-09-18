e56e233bdb1d13748fffeb31a642fc29528dc237
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-18 11:08:00 +0300

    task09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  buttonEl: document.querySelector('.change-color'),
+  textEl: document.querySelector('.color'),
+};
+
+refs.buttonEl.addEventListener('click', onButtonClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onButtonClick() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.textEl.textContent = `${getRandomHexColor()}`;
 }
